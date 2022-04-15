8e53b496969efbfa4ee977e647c09eadd6b76a07
SplitRabbit <jue.kun.liu@gmail.com>
2022-04-15 15:37:30 -0400

    added fade out to reach main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,16 +12,32 @@ function showFade(element) {
         }, 30);
 };
 
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        op -= op * 0.1;
+    }, 30);
+};
+
 //fade in image and text
 function showEntrance() {
     let entranceimg = document.getElementById("entrance-image")
     let entrancetext1 = document.getElementById("entrance-text-1")
     let entrancetext2 = document.getElementById("entrance-text-2")
+    let entrancepage = document.getElementById("entrance-page")
 setTimeout(() => {
     showFade(entranceimg);
     setTimeout(() => {
         showFade(entrancetext1);
         showFade(entrancetext2);
+        setTimeout(() => {
+            fade(entrancepage);
+        }, 3000);
     }, 500);
 }, 500);
 };
